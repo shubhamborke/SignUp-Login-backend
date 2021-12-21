abcbe1ba16dcbf5efd9a66f53876db8d2b56686a
shubhamborke <shubham.borke@gmail.com>
2021-12-21 17:36:52 +0530

    Added backend

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const uri =
+  "mongodb+srv://ShubhamBorke:shubhamsj@firstcluster.ezspt.mongodb.net/intern?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/signup", (req, res) => {
+  res.send("hello");
+});
+
+app.post("/signup", cors(), async (req, res) => {
+  const result = await req.body;
+  const { name, email, mobile, age, dateOfBirth } = result;
+  try {
+    await client.connect();
+    const checkExist = await client.db("intern").collection("intern").findOne({
+      name: name,
+      email: email,
+      mobile: mobile,
+      age: age,
+      dateOfBirth: dateOfBirth,
+    });
+    if (checkExist === null) {
+      await client.db("intern").collection("intern").insertOne(result);
+      res.send("created");
+    } else {
+      res.send("exist");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", cors(), async (req, res) => {
+  const result = await req.body;
+  console.log(result);
+  await client.connect();
+  const dataCollection = await client.db("intern").collection("intern").findOne({name: result.username, password: result.password})
+  if(dataCollection !== null){
+      res.send(dataCollection)
+  }else{
+      res.send("wrong")
+  }
+});
+app.listen(4000);
